6dab9643096c4c6431ef667f5f3ded7e63d3375f
Rodman Varona <notrodmanvarona@gmail.com>
2026-03-02 12:01:18 +0800

    Create LAB1.py

---
+++ LAB1.py
@@ -0,0 +1,22 @@
+Name = "RODMAN"
+Age = 19
+FavColor = "RED"
+Address  = "BITADTON SUR"
+Program = "BSCS"
+FavSubject = "CPROG3"
+Hobby = "SLEEPING" 
+University = "UA-TLMC"
+Nickname = "ROD"
+Instructor = "JUVELYN SAMILLANO"
+
+print("Name: ", Name)
+print("Age: ", Age)
+print("Favorite Color: ", FavColor)
+print("Address: ", Address)
+print("Program: ", Program)
+print("Favorite Subject: ", FavSubject)
+print("Hobby: ", Hobby)
+print("University: ", University)
+print("Nickname: ", Nickname)
+print("Instructor: ", Instructor)
+
